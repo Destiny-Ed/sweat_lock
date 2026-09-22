375177a30d0df50059cf3fe48fd62baea79f7d37
Destiny Ed <dikeachaeze@gmail.com>
2026-09-22 13:41:30 +0100

    Phase 1: Wire HiveService into main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:sweat_lock/core/constant.dart';
 import 'package:sweat_lock/core/theme.dart';
+import 'package:sweat_lock/data/local/hive_service.dart';
 import 'package:sweat_lock/injection.dart';
 import 'package:sweat_lock/presentation/views/auth/login.dart';
+import 'package:sweat_lock/presentation/views/main_activity.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-
-  // NotificationService().init();
+  await HiveService.init();
   runApp(const MyApp());
 }
 
@@ -19,6 +18,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isOnboarded = HiveService.isOnboardingComplete();
+
     return MultiProvider(
       providers: providers(context),
       child: MaterialApp(
@@ -26,8 +27,8 @@ class MyApp extends StatelessWidget {
         title: appName,
         theme: AppTheme.lightTheme,
         darkTheme: AppTheme.darkTheme,
-        themeMode: ThemeMode.light,
-        home: const LoginView(),
+        themeMode: ThemeMode.system,
+        home: isOnboarded ? const MainActivity() : const LoginView(),
       ),
     );
   }
