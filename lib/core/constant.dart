36615a568af3bd031ad7f1441026c1fc46109f2a
Destiny Ed <dikeachaeze@gmail.com>
2026-09-23 16:33:14 +0100

    Add music genres + emergency unlock duration

--- lib/core/constant.dart
+++ lib/core/constant.dart
@@ -6,6 +6,9 @@ const String defaultExercise = 'push-ups';
 const int emergencyUnlocksPerDay = 1;
 const int unlockDurationMinutes = 30;
 
+/// Emergency unlock lasts this many minutes (can differ from normal).
+const int emergencyUnlockDurationMinutes = 45;
+
 /// Free usage window before iOS system shield is applied (minutes).
 const int iosUsageLimitMinutes = 5;
 
@@ -23,6 +26,19 @@ const List<String> supportedExercises = [
   'squats',
   'sit-ups',
   'jumping jacks',
+];
+
+const List<String> supportedMusicGenres = [
+  'pop',
+  'hip hop / rap',
+  'rock',
+  'electronic / EDM',
+  'latin',
+  'R&B',
+  'indie',
+  'gospel',
+  'classical / instrumental',
+  'afrobeats',
 ];
 
 const String blockedAppsBox = 'blocked_apps';
